85d321af7532b3f189788731f68dc16aae4f4dda
Icaro <icaronunes@hotmail.com>
2019-04-07 03:48:13 -0300

    alterado xml do tvshow

--- app/src/main/java/fragment/FragmentBase.kt
+++ app/src/main/java/fragment/FragmentBase.kt
@@ -4,19 +4,18 @@ import android.support.v4.app.Fragment
 import rx.subscriptions.CompositeSubscription
 
 open class FragmentBase: Fragment() {
-
+    
     protected var subscriptions = CompositeSubscription()
-
+    
     override fun onResume() {
         super.onResume()
         subscriptions = CompositeSubscription()
-
+        
     }
-
+    
     override fun onPause() {
         super.onPause()
         subscriptions.unsubscribe()
         subscriptions.clear()
     }
-
 }
