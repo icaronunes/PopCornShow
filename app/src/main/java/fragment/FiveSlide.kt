9b713e7898c1d5f0f79f3918887be8bd49775b19
icaro <icaronunes@hotmail.com>
2019-08-15 15:08:36 -0300

    convertido Intro para Kotlin

---
+++ app/src/main/java/fragment/FiveSlide.kt
@@ -0,0 +1,31 @@
+package fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+import br.com.icaro.filme.R
+
+/**
+ * Created by icaro on 03/12/16.
+ */
+class FiveSlide : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.intro, container, false)
+        val title = view.findViewById<View>(R.id.intro_tite) as TextView
+        title.text = resources.getString(R.string.app_name)
+
+        val subtitle = view.findViewById<View>(R.id.intro_subtitle) as TextView
+        subtitle.text = resources.getText(R.string.subtitle_intro_5)
+
+        val imageView = view.findViewById<View>(R.id.intro_img) as ImageView
+        imageView.setImageResource(R.drawable.intro_icon)
+        return view
+
+    }
+}
---
+++ app/src/main/java/fragment/SecondSlide.kt
@@ -0,0 +1,31 @@
+package fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+import br.com.icaro.filme.R
+
+/**
+ * Created by icaro on 21/11/16.
+ */
+class SecondSlide : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.intro, container, false)
+        val title = view.findViewById<View>(R.id.intro_tite) as TextView
+        title.text = "Links"
+
+        val subtitle = view.findViewById<View>(R.id.intro_subtitle) as TextView
+        subtitle.text = resources.getText(R.string.subtitle_intro_2)
+
+        val imageView = view.findViewById<View>(R.id.intro_img) as ImageView
+        imageView.setImageResource(R.drawable.intro_link)
+        return view
+
+    }
+}
