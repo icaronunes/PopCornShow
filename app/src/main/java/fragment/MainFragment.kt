4bc54b15435585ae9787dbebf00c5a1411609bb1
Icaro <icaronunes@hotmail.com>
2019-08-17 01:47:39 -0300

    mudando butao para Chip no Main

--- app/src/main/java/fragment/MainFragment.kt
+++ app/src/main/java/fragment/MainFragment.kt
@@ -4,6 +4,7 @@ import adapter.MovieMainAdapter
 import adapter.TvShowMainAdapter
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +17,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.icaro.filme.R
 import br.com.icaro.filme.R.string.filmes_main
+import com.google.android.material.chip.Chip
 import domain.Api
 import domain.FilmeService
 import domain.ListaSeries
@@ -23,6 +25,8 @@ import domain.movie.ListaFilmes
 import domain.movie.ListaItemFilme
 import info.movito.themoviedbapi.TvResultsPage
 import info.movito.themoviedbapi.model.core.MovieResultsPage
+import kotlinx.android.synthetic.main.filmes_main.*
+import kotlinx.android.synthetic.main.tvshow_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -41,10 +45,18 @@ import java.util.Arrays.asList
  * Created by icaro on 23/08/16.
  */
 class MainFragment : Fragment() {
-
-    private var buttonFilme: List<String>? = null
-    private var buttonTvshow: List<String>? = null
     private var tipo: Int = 0
+
+    companion object {
+
+        fun newInstance(informacoes: Int): Fragment {
+            val fragment = MainFragment()
+            val bundle = Bundle()
+            bundle.putInt(Constantes.ABA, informacoes)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
 
     private lateinit var tvshow_popular_main: RecyclerView
     private lateinit var recycle_tvshowtoday_main: RecyclerView
@@ -53,10 +65,6 @@ class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        buttonFilme = ArrayList(asList(getString(R.string.now_playing),
-                getString(R.string.upcoming), getString(R.string.populares), getString(R.string.top_rated)))
-        buttonTvshow = ArrayList(asList(getString(R.string.air_date),
-                getString(R.string.today), getString(R.string.populares), getString(R.string.top_rated)))
         if (arguments != null) {
             tipo = arguments!!.getInt(Constantes.ABA)
         }
@@ -64,108 +72,74 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
         if (tipo == R.string.tvshow_main) {
             setScrollTvshowButton()
-        }
-        if (tipo == R.string.filmes_main) {
+        } else {
             setScrollFilmeButton()
         }
-
     }
 
     private fun setScrollFilmeButton() {
-        for (i in 0..3) {
-            val view = activity!!.layoutInflater.inflate(R.layout.main_botton, view as ViewGroup?, false)
-            val linearLayout = getView()!!.findViewById<View>(R.id.scroll_filme_button_main) as LinearLayout
-            val layoutScroll = view.findViewById<View>(R.id.layout_main_button)
+        chip_group_movie.setOnCheckedChangeListener { chipGroup, id ->
+            Log.d(this.javaClass.name, "$chipGroup - $id")
+            when (id) {
+                R.id.chip_now_playing -> {
+                    startActivity(Intent(activity, FilmesActivity::class.java).apply {
+                        putExtra(Constantes.ABA, R.string.now_playing)
+                        putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.now_playing)
+                    })
+                }
 
-            val button = view.findViewById<View>(R.id.button_main) as Button
-            button.text = buttonFilme!![i]
+                R.id.chip_upcoming -> {
+                    startActivity(Intent(activity, FilmesActivity::class.java).apply {
+                        putExtra(Constantes.ABA, R.string.upcoming)
+                        putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.upcoming)
+                    })
+                }
 
-            button.setOnClickListener {
-                when (i) {
+                R.id.chip_populares -> {
+                    startActivity(Intent(activity, FilmesActivity::class.java).apply {
+                        putExtra(Constantes.ABA, R.string.populares)
+                        putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.populares)
+                    })
+                }
 
-                    0 -> {
-
-                        val intent = Intent(activity, FilmesActivity::class.java)
-                        intent.putExtra(Constantes.ABA, R.string.now_playing)
-                        intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.now_playing)
-                        startActivity(intent)
-                    }
-
-                    1 -> {
-
-                        val intent = Intent(activity, FilmesActivity::class.java)
-                        intent.putExtra(Constantes.ABA, R.string.upcoming)
-                        intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.upcoming)
-                        startActivity(intent)
-                    }
-
-                    2 -> {
-
-                        val intent = Intent(activity, FilmesActivity::class.java)
-                        intent.putExtra(Constantes.ABA, R.string.populares)
-                        intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.populares)
-                        startActivity(intent)
-                    }
-
-                    3 -> {
-
-                        val intent = Intent(activity, FilmesActivity::class.java)
-                        intent.putExtra(Constantes.ABA, R.string.top_rated)
-                        intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.top_rated)
-                        startActivity(intent)
-                    }
+                R.id.chip_top_rated -> {
+                    startActivity(Intent(activity, FilmesActivity::class.java).apply {
+                        putExtra(Constantes.ABA, R.string.top_rated)
+                        putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.top_rated)
+                    })
                 }
             }
-
-            linearLayout.addView(layoutScroll)
         }
-
     }
 
     private fun setScrollTvshowButton() {
-        for (i in 0..3) {
-            val view = activity!!.layoutInflater.inflate(R.layout.main_botton, view as ViewGroup?, false)
-            val linearLayout = getView()!!.findViewById<View>(R.id.scroll_tvshow_button_main) as LinearLayout
-            val layoutScroll = view.findViewById<View>(R.id.layout_main_button)
+        chip_group_tvshow.setOnCheckedChangeListener { chipGroup, id ->
+            Log.d(this.javaClass.name, "$chipGroup - $id")
+            when (id) {
+                R.id.chip_air_data -> {
+                    val intent = Intent(activity, TvShowsActivity::class.java)
+                    intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.air_date)
+                    startActivity(intent)
+                }
+                R.id.chip_today -> {
+                    val intent = Intent(activity, TvShowsActivity::class.java)
+                    intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.today)
+                    startActivity(intent)
+                }
+                R.id.chip_populares_tvshow -> {
+                    val intent = Intent(activity, TvShowsActivity::class.java)
+                    intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.populares)
+                    startActivity(intent)
+                }
 
-            val button = view.findViewById<View>(R.id.button_main) as Button
-            button.text = buttonTvshow!![i]
-
-            button.setOnClickListener {
-                when (i) {
-
-                    0 -> {
-
-                        val intent = Intent(activity, TvShowsActivity::class.java)
-                        intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.air_date)
-                        startActivity(intent)
-                    }
-                    1 -> {
-
-                        val intent = Intent(activity, TvShowsActivity::class.java)
-                        intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.today)
-                        startActivity(intent)
-                    }
-                    2 -> {
-
-                        val intent = Intent(activity, TvShowsActivity::class.java)
-                        intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.populares)
-                        startActivity(intent)
-                    }
-
-                    3 -> {
-
-                        val intent = Intent(activity, TvShowsActivity::class.java)
-                        intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.top_rated)
-                        startActivity(intent)
-                    }
+                R.id.chip_top_rated_tvshow -> {
+                    val intent = Intent(activity, TvShowsActivity::class.java)
+                    intent.putExtra(Constantes.NAV_DRAW_ESCOLIDO, R.string.top_rated)
+                    startActivity(intent)
                 }
             }
-
-            linearLayout.addView(layoutScroll)
         }
     }
 
@@ -270,18 +244,5 @@ class MainFragment : Fragment() {
 
     private fun setScrollMovieOntheAir(airDay: ListaFilmes) {
         recycle_movie_popular_main.adapter = MovieMainAdapter(activity, airDay)
-    }
-
-
-    companion object {
-
-        fun newInstance(informacoes: Int): Fragment {
-            val fragment = MainFragment()
-            val bundle = Bundle()
-            bundle.putInt(Constantes.ABA, informacoes)
-            fragment.arguments = bundle
-
-            return fragment
-        }
     }
 }
