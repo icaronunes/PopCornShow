3654d2a8b04b97e47fc4862522634633533b525e
Icaro Nunes <icarornunes@gmail.com>
2020-06-06 16:32:36 -0300

    criado extension para Any

---
+++ app/src/main/java/utils/AnyExt.kt
@@ -0,0 +1,29 @@
+
+private val ZERO = 0
+private val EMPTY_STRING = ""
+
+infix fun <T, R> T.ifValid(function: () -> R?) = if (hasValue) function.invoke() else null
+
+fun <T> forEachArg(vararg any: T, function: T.() -> Unit) = any.forEach { function.invoke(it) }
+
+infix fun <T> T.otherWise(other: T) = if (hasValue) this else other
+
+infix fun <T> T?.safely(function: (T) -> Unit) {
+    if (hasValue && this != null) function.invoke(this)
+}
+
+val <T> T.hasValue
+    get() = when (this) {
+        null -> false
+        false -> false
+        ZERO -> false
+        EMPTY_STRING -> false
+        ZERO.toString() -> false
+        ZERO.toLong() -> false
+        ZERO.toDouble() -> false
+        ZERO.toFloat() -> false
+        arrayListOf<T>() -> false
+        listOf<T>() -> false
+        {} -> false
+        else -> true
+    }
