af0ccf6aa5984adf822a99315fc1ff269cd2c921
Icaro Nunes <icarornunes@gmail.com>
2020-06-14 15:23:57 -0300

    fix - code para remover nullble

--- app/src/main/java/busca/SearchMultiActivity.kt
+++ app/src/main/java/busca/SearchMultiActivity.kt
@@ -33,106 +33,109 @@ import utils.visible
  * Created by icaro on 08/07/16.
  */
 class SearchMultiActivity : BaseActivityKt() {
-    //Colocar paginação na lista
-    private val recyclerView: RecyclerView by lazy {
-            recycleView_search.apply {
-            layoutManager = LinearLayoutManager(context)
-            itemAnimator = DefaultItemAnimator()
-            setHasFixedSize(true)
-        }
-    }
-    private val model: SearchMultiModelView by lazy {
-        createViewModel(SearchMultiModelView::class.java, this)
-    }
-    private var query = ""
+	//Colocar paginação na lista
+	private val recyclerView: RecyclerView by lazy {
+		recycleView_search.apply {
+			layoutManager = LinearLayoutManager(context)
+			itemAnimator = DefaultItemAnimator()
+			setHasFixedSize(true)
+		}
+	}
+	private val model: SearchMultiModelView by lazy {
+		createViewModel(SearchMultiModelView::class.java, this)
+	}
+	private var query = ""
 
-    public override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(layout.search_layout)
-        setUpToolBar()
-        setupNavDrawer()
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        query = intent.getStringExtra(SearchManager.QUERY)
-        supportActionBar!!.title = query
-        observers()
-        if (Intent.ACTION_VIEW == intent.action) { //TODO pra que server isso?
-            val intent: Intent
-            when {
-                getIntent().data.lastPathSegment.equals(MOVIE.type, ignoreCase = true) -> {
-                    intent = Intent(this, MovieDetailsActivity::class.java)
-                    val id = getIntent().extras.getString(SearchManager.EXTRA_DATA_KEY).toInt() //ID
-                    intent.putExtra(Constant.FILME_ID, id)
-                    intent.flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
-                    startActivity(intent)
-                    finish()
-                    return
-                }
-                getIntent().data.lastPathSegment.equals(TV.type, ignoreCase = true) -> {
-                    val id = getIntent().extras.getString(SearchManager.EXTRA_DATA_KEY).toInt() //ID
-                    intent = Intent(this, TvShowActivity::class.java)
-                    intent.putExtra(Constant.TVSHOW_ID, id)
-                    intent.flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
-                    startActivity(intent)
-                    finish()
-                    return
-                }
-                getIntent().data.lastPathSegment.equals(PERSON.type, ignoreCase = true) -> {
-                    intent = Intent(this, PersonActivity::class.java)
-                    val id = getIntent().extras.getString(SearchManager.EXTRA_DATA_KEY).toInt() //ID
-                    intent.putExtra(Constant.PERSON_ID, id)
-                    intent.flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
-                    startActivity(intent)
-                    finish()
-                    return
-                }
-            }
-        } else {
-            if (isNetWorkAvailable(baseContext)) {
-                model.fetchData(query)
-            } else {
-                snack(adView, getString(R.string.tryagain)) {
-                    model.fetchData(query)
-                }
-            }
-        }
-    }
+	public override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(layout.search_layout)
+		setUpToolBar()
+		setupNavDrawer()
+		supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+		query = intent.getStringExtra(SearchManager.QUERY)
+		supportActionBar!!.title = query
+		observers()
+		if (Intent.ACTION_VIEW == intent.action) { //TODO pra que server isso?
+			when {
+				intent.data?.lastPathSegment.equals(MOVIE.type, ignoreCase = true) -> {
+					startActivity(Intent(this, MovieDetailsActivity::class.java).apply {
+						val id = intent?.extras?.getString(SearchManager.EXTRA_DATA_KEY)?.toInt() ?: -1
+						intent.putExtra(Constant.FILME_ID, id)
+						intent.flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
+					})
+					finish()
+					return
+				}
+				intent.data?.lastPathSegment.equals(TV.type, ignoreCase = true) -> {
+					startActivity(Intent(this, TvShowActivity::class.java).apply {
+						intent.putExtra(
+							Constant.TVSHOW_ID,
+							intent?.extras?.getString(SearchManager.EXTRA_DATA_KEY)?.toInt() ?: -1
+						)
+						intent.flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
+					})
+					finish()
+					return
+				}
+				intent.data?.lastPathSegment.equals(PERSON.type, ignoreCase = true) -> {
+					startActivity(Intent(this, PersonActivity::class.java).apply {
+						intent.putExtra(
+							Constant.PERSON_ID,
+							intent?.extras?.getString(SearchManager.EXTRA_DATA_KEY)?.toInt() ?: -1
+						)
+						intent.flags = Intent.FLAG_ACTIVITY_FORWARD_RESULT
+					})
+					finish()
+					return
+				}
+			}
+		} else {
+			if (isNetWorkAvailable(baseContext)) {
+				model.fetchData(query)
+			} else {
+				snack(adView, getString(R.string.tryagain)) {
+					model.fetchData(query)
+				}
+			}
+		}
+	}
 
-    private fun observers() {
-        model.response.observe(this, Observer {
-            when (it) {
-                is BaseRequest.Success -> {
-                    model.setLoading(false)
-                    fillRecycler(it.result as SearchMulti)
-                }
-                is BaseRequest.Failure -> {
-                    snack(adView, getString(R.string.tryagain)) {
-                        model.fetchData(query)
-                    }
-                }
-                is BaseRequest.Loading -> {
-                    setLoading(it.loading)
-                }
-            }
-        })
-    }
+	private fun observers() {
+		model.response.observe(this, Observer {
+			when (it) {
+				is BaseRequest.Success -> {
+					model.setLoading(false)
+					fillRecycler(it.result as SearchMulti)
+				}
+				is BaseRequest.Failure -> {
+					snack(adView, getString(R.string.tryagain)) {
+						model.fetchData(query)
+					}
+				}
+				is BaseRequest.Loading -> {
+					setLoading(it.loading)
+				}
+			}
+		})
+	}
 
-    private fun setLoading(loading: Boolean) {
-        progress.visibility = if (loading) View.VISIBLE else View.GONE
-    }
+	private fun setLoading(loading: Boolean) {
+		progress.visibility = if (loading) View.VISIBLE else View.GONE
+	}
 
-    private fun fillRecycler(it: SearchMulti) {
-        if (it.results.isEmpty()) {
-            text_search_empty.visible()
-            model.setLoading(false)
-        } else {
-            text_search_empty.gone()
-            model.setLoading(false)
-            recyclerView.adapter = SearchDelegateAdapter(this@SearchMultiActivity, it)
-        }
-    }
+	private fun fillRecycler(it: SearchMulti) {
+		if (it.results.isEmpty()) {
+			text_search_empty.visible()
+			model.setLoading(false)
+		} else {
+			text_search_empty.gone()
+			model.setLoading(false)
+			recyclerView.adapter = SearchDelegateAdapter(this@SearchMultiActivity, it)
+		}
+	}
 
-    override fun onResume() {
-        super.onResume()
-        setAdMob(adView)
-    }
+	override fun onResume() {
+		super.onResume()
+		setAdMob(adView)
+	}
 }
