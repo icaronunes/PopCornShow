c7b78111a57aa9e8b7969d0829ff111d050e96b3
Icaro Nunes <icarornunes@gmail.com>
2020-07-15 01:47:18 -0300

    small: usando bindBundle

--- app/src/main/java/elenco/ElencoAdapter.kt
+++ app/src/main/java/elenco/ElencoAdapter.kt
@@ -1,5 +1,6 @@
 package elenco
 
+import Layout
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.RecyclerView
 import br.com.icaro.filme.R
 import domain.CastItem
@@ -17,37 +19,41 @@ import utils.setPicassoWithCache
 /**
  * Created by icaro on 24/07/16.
  */
-class ElencoAdapter(private val context: Context, private val casts: List<CastItem?>?) : RecyclerView.Adapter<ElencoAdapter.ElencoViewHolder>() {
+class ElencoAdapter(private val context: Context, private val casts: List<CastItem?>?) :
+	RecyclerView.Adapter<ElencoAdapter.ElencoViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElencoViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.elenco_list_adapter, parent, false)
-        return ElencoViewHolder(view)
-    }
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElencoViewHolder {
+		val view = LayoutInflater.from(context).inflate(Layout.elenco_list_adapter, parent, false)
+		return ElencoViewHolder(view)
+	}
 
-    override fun onBindViewHolder(holder: ElencoViewHolder, position: Int) {
-        val (_, character, _, _, name, profilePath, id) = casts?.get(position)!!
-        holder.elencoCharacter.text = character
+	override fun onBindViewHolder(holder: ElencoViewHolder, position: Int) {
+		val (_, character, _, _, name, profilePath, id) = casts?.get(position)!!
+		holder.elencoCharacter.text = character
 
-        holder.elencoNome.text = name
+		holder.elencoNome.text = name
+		holder.imgElenco.setPicassoWithCache(profilePath, 2, img_erro = R.drawable.person)
 
-        holder.imgElenco.setPicassoWithCache(profilePath, 2, img_erro = R.drawable.person)
+		holder.itemView.setOnClickListener {
+			context.startActivity(Intent(context, PersonActivity::class.java).apply {
+				putExtras(
+					bundleOf(
+						Constant.PERSON_ID to id,
+						Constant.NOME_PERSON to name
+					)
+				)
+			})
+		}
+	}
 
-        holder.itemView.setOnClickListener {
-            context.startActivity(Intent(context, PersonActivity::class.java).apply {
-                putExtra(Constant.PERSON_ID, id)
-                putExtra(Constant.NOME_PERSON, name)
-            })
-        }
-    }
+	override fun getItemCount(): Int {
+		return casts?.size ?: 0
+	}
 
-    override fun getItemCount(): Int {
-        return casts?.size ?: 0
-    }
+	inner class ElencoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    inner class ElencoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        val elencoNome: TextView = itemView.findViewById(R.id.elenco_nome)
-        val elencoCharacter: TextView = itemView.findViewById(R.id.elenco_character)
-        val imgElenco: ImageView = itemView.findViewById(R.id.img_elenco)
-    }
+		val elencoNome: TextView = itemView.findViewById(R.id.elenco_nome)
+		val elencoCharacter: TextView = itemView.findViewById(R.id.elenco_character)
+		val imgElenco: ImageView = itemView.findViewById(R.id.img_elenco)
+	}
 }
