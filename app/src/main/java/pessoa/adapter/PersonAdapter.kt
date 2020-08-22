3ce72cfbc8cd46eb7b7c7331263943254cc40571
Icaro <icaro.nunes_desenv@hotmail.com>
2020-08-22 15:40:26 -0300

    refatorado para separar em fragment em Person

--- app/src/main/java/pessoa/adapter/PersonAdapter.kt
+++ app/src/main/java/pessoa/adapter/PersonAdapter.kt
@@ -1,81 +1,51 @@
-package pessoa.adapter;
+package pessoa.adapter
 
-import android.content.Context;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-
-import br.com.icaro.filme.R;
-import fragment.FragmentError;
-import pessoa.fragment.PersonFragmentMovie;
-import pessoa.fragment.PersonFragmentPhoto;
-import pessoa.fragment.PersonFragmentProduction;
-import pessoa.fragment.PersonFragmentProfile;
-import pessoa.fragment.PersonFragmentTv;
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import br.com.icaro.filme.R.*
+import fragment.FragmentError
+import pessoa.fragment.PersonFragmentMovie
+import pessoa.fragment.PersonFragmentPhoto
+import pessoa.fragment.PersonFragmentProduction
+import pessoa.fragment.PersonFragmentProfile
+import pessoa.fragment.PersonFragmentTv
 
 /**
  * Created by icaro on 18/08/16.
  */
-public class PersonAdapter extends FragmentPagerAdapter {
-    private Context context;
-    private Boolean error;
+class PersonAdapter(
+	private val context: Context,
+	supportFragmentManager: FragmentManager?,
+	private val error: Boolean
+) : FragmentPagerAdapter(
+	supportFragmentManager!!, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+) {
+	override fun getCount() = if (error) 1 else 5
+	override fun getItem(position: Int): Fragment {
+		if (error) return FragmentError()
+		return when (position) {
+			0 -> PersonFragmentTv()
+			1 -> PersonFragmentMovie()
+			2 -> PersonFragmentProfile()
+			3 -> PersonFragmentPhoto()
+			4 -> PersonFragmentProduction()
+			else -> PersonFragmentTv()
+		}
+	}
 
-    public PersonAdapter(Context context, FragmentManager supportFragmentManager, Boolean error) {
-        super(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        this.context = context;
-        this.error = error;
-    }
+	override fun getPageTitle(position: Int): CharSequence? {
+		if (error) return "Error :("
+		return when (position) {
+			0 -> context.getString(string.tvshow)
+			1 -> context.getString(string.filme)
+			2 -> context.getString(string.person)
+			3 -> context.getString(string.imagem_person)
+			4 -> context.getString(string.producao)
+			else -> "Error"
+		}
+	}
 
-    @Override
-    public Fragment getItem(int position) {
-        if(error) return new FragmentError();
 
-        if (position == 0) {
-            return new PersonFragmentTv();
-        }
-        if (position == 1) {
-            return new PersonFragmentMovie();
-        }
-        if (position == 2) {
-            return new PersonFragmentProfile();
-        }
-        if (position == 3) {
-            return new PersonFragmentPhoto();
-        }
-        if (position == 4) {
-            return new PersonFragmentProduction();
-        }
-
-        return new PersonFragmentTv();
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-        if(error) return "Error :(";
-
-        if (position == 0) {
-            return context.getString(R.string.tvshow);
-        }
-        if (position == 1) {
-            return context.getString(R.string.filme);
-        }
-        if (position == 2) {
-            return context.getString(R.string.person);
-        }
-        if (position == 3) {
-            return context.getString(R.string.imagem_person);
-        }
-        if (position == 4) {
-            return context.getString(R.string.producao);
-        } else {
-            return "Error";
-        }
-    }
-
-    @Override
-    public int getCount() {
-        if(error) return 1;
-        return 5;
-    }
 }
