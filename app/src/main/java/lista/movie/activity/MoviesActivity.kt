69d846f45e18b0af73a7dd3e428930a114f5be6e
Icaro <icaro.nunes_desenv@hotmail.com>
2020-10-05 21:47:57 -0300

    pequenas mudança nas listas

--- app/src/main/java/lista/movie/activity/MoviesActivity.kt
+++ app/src/main/java/lista/movie/activity/MoviesActivity.kt
@@ -1,37 +1,41 @@
 package lista.movie.activity
 
-import activity.BaseActivity
+import Layout
+import activity.BaseActivityAb
 import android.os.Bundle
 import br.com.icaro.filme.R
 import br.com.icaro.filme.R.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import lista.movie.fragment.MoviesFragment
 import utils.Api
 import utils.Constant
 
-class MoviesActivity : BaseActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(layout.activity_list_main)
-        setUpToolBar()
-        setupNavDrawer()
-        getTitleChoose()
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(id.container_list_main, MoviesFragment().apply {
-                    arguments = intent.extras })
-                .commit()
-        }
-    }
+@ExperimentalCoroutinesApi
+class MoviesActivity(override var layout: Int = Layout.activity_list_main) : BaseActivityAb() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(layout)
+		setUpToolBar()
+		setupNavDrawer()
+		getTitleChoose()
+		supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+		if (savedInstanceState == null) {
+			supportFragmentManager
+				.beginTransaction()
+				.add(id.container_list_main, MoviesFragment().apply {
+                    arguments = intent.extras
+                })
+				.commit()
+		}
+	}
 
-    private fun getTitleChoose() {
-        supportActionBar!!.title = when (intent.getStringExtra(Constant.NAV_DRAW_ESCOLIDO)) {
+	private fun getTitleChoose() {
+		supportActionBar?.title = when (intent.getStringExtra(Constant.NAV_DRAW_ESCOLIDO)) {
             Api.TYPESEARCH.MOVIE.now -> getString(R.string.now_playing)
             Api.TYPESEARCH.MOVIE.upComing -> getString(R.string.upcoming)
             Api.TYPESEARCH.MOVIE.popular -> getString(R.string.populares)
             Api.TYPESEARCH.MOVIE.bestScore -> getString(R.string.top_rated)
-            else -> getString(R.string.app_name)
-        }
-    }
+			else -> getString(R.string.app_name)
+		}
+	}
 }
--- app/src/main/java/lista/tvshow/activity/TvShowsActivity.kt
+++ app/src/main/java/lista/tvshow/activity/TvShowsActivity.kt
@@ -3,47 +3,48 @@ package lista.tvshow.activity
 /**
  * Created by icaro on 14/09/16.
  */
-
-import activity.BaseActivity
+import Layout
+import activity.BaseActivityAb
 import android.os.Bundle
 import android.view.Menu
 import br.com.icaro.filme.R
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import lista.tvshow.fragment.TvShowsFragment
 import utils.Api
 import utils.Constant
 
-class TvShowsActivity : BaseActivity() {
+@ExperimentalCoroutinesApi
+class TvShowsActivity(override var layout: Int = Layout.activity_list_main) : BaseActivityAb() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(layout)
+		setUpToolBar()
+		setupNavDrawer()
+		getTitleChoose()
+		supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_main)
-        setUpToolBar()
-        setupNavDrawer()
-        getTitleChoose()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        if (savedInstanceState == null) {
-            val tvShowsFragment = TvShowsFragment()
-            tvShowsFragment.arguments = intent.extras
-            supportFragmentManager
+		if (savedInstanceState == null) {
+			val tvShowsFragment = TvShowsFragment()
+			tvShowsFragment.arguments = intent.extras
+			supportFragmentManager
                 .beginTransaction()
-                .add(R.id.container_list_main, tvShowsFragment)
-                .commit()
-        }
-    }
+				.add(R.id.container_list_main, tvShowsFragment)
+				.commit()
+		}
+	}
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		menuInflater.inflate(R.menu.menu, menu)
+		return true
+	}
 
-    private fun getTitleChoose() {
-        supportActionBar!!.title = when (intent.getStringExtra(Constant.NAV_DRAW_ESCOLIDO)) {
+	private fun getTitleChoose() {
+		supportActionBar?.title = when (intent.getStringExtra(Constant.NAV_DRAW_ESCOLIDO)) {
             Api.TYPESEARCH.TVSHOW.toDay -> getString(R.string.today)
             Api.TYPESEARCH.TVSHOW.week -> getString(R.string.air_date_main)
             Api.TYPESEARCH.TVSHOW.popular -> getString(R.string.populares)
             Api.TYPESEARCH.TVSHOW.bestScore -> getString(R.string.top_rated)
-            else -> getString(R.string.app_name)
-        }
-    }
+			else -> getString(R.string.app_name)
+		}
+	}
 }
